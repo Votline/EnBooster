fd239ae6c9217a42f56077af34d32c5fef3ebb0e
Votline <167365107+Votline@users.noreply.github.com>
2026-06-25 10:01:08 +0300

    feat: pingpong bot (#11)
    * register tg bot and handle 'ping' message
    
    - Add 'telebot' and 'zap' package
    - Create telebot bot with token from env
    - Init zap logger
    
    * chore: add telebot and zap licenses
    
    * docs: add comments to main.go

---
+++ go.mod
@@ -0,0 +1,9 @@
+module enbstr
+
+go 1.26.3
+
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.28.0 // indirect
+	gopkg.in/telebot.v3 v3.3.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,63 @@
+// package main creates a Telegram bot and zap logger.
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"go.uber.org/zap"
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	log, err := createLogger()
+	if err != nil {
+		panic(err)
+	}
+	defer log.Sync()
+
+	b, err := createBot()
+	if err != nil {
+		log.Fatal("Failed to create bot", zap.Error(err))
+	}
+
+	b.Handle("/ping", func(c tele.Context) error {
+		return c.Send("pong")
+	})
+
+	log.Info("Bot is successfully created")
+
+	b.Start()
+}
+
+// createLogger creates a production zap logger.
+func createLogger() (*zap.Logger, error) {
+	const op = "main.createLogger"
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed to create logger: %w", op, err)
+	}
+
+	return logger, nil
+}
+
+// createBot creates a Telegram bot.
+// It uses the BOT_TOKEN environment variable.
+// Timeout is set to 10 seconds.
+func createBot() (*tele.Bot, error) {
+	const op = "main.createBot"
+
+	pref := tele.Settings{
+		Token:  os.Getenv("BOT_TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed to create bot: %w", op, err)
+	}
+
+	return b, nil
+}
