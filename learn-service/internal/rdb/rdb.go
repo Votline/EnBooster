637d9cdf6f2fe7b8f179e0e822efb5d4d83cb59c
Votline <quogg2@gmail.com>
2026-06-30 21:07:44 +0300

    feat: implement adaptive-caching with singleflight
    - Use Lua-script for atomically increment and get counter
    - Cache tasks if counter > 30
    - Use singleflight for get tasks

--- learn-service/internal/rdb/rdb.go
+++ learn-service/internal/rdb/rdb.go
@@ -7,12 +7,15 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"unsafe"
 
 	"github.com/go-redis/redis/v8"
+	"golang.org/x/sync/singleflight"
 )
 
 type RDB struct {
-	rdb *redis.Client
+	rdb     *redis.Client
+	sfGroup singleflight.Group
 }
 
 func NewRDB() (*RDB, error) {
@@ -38,23 +41,92 @@ func (r *RDB) Close() error {
 	return r.rdb.Close()
 }
 
-func (r *RDB) CacheTasks(ctx context.Context, key, value string) error {
+var incrAndExpire = redis.NewScript(`
+local current = redis.call("INCR", KEYS[1])
+if current == 1 then
+    redis.call("EXPIRE", KEYS[1], ARGV[1])
+end
+return current
+`)
+
+func (r *RDB) CacheTasks(ctx context.Context, bufCount, bufCache *[]byte, value string) error {
 	const op = "rdb.CacheTasks"
 
-	if err := r.rdb.Set(ctx, key, value, 30*time.Minute).Err(); err != nil {
+	BuildKey("tasks:count:", bufCount)
+	countKey := unsafe.String(unsafe.SliceData(*bufCount), len(*bufCount))
+
+	BuildKey("tasks:cache:", bufCache)
+	cacheKey := unsafe.String(unsafe.SliceData(*bufCache), len(*bufCache))
+
+	count, err := incrAndExpire.Run(ctx, r.rdb, []string{countKey}, 1800).Int64()
+	if err != nil {
+		return fmt.Errorf("%s: redis incr and expire: %w", op, err)
+	}
+
+	if count < 10 {
+		return nil
+	}
+
+	if err := r.rdb.Set(ctx, cacheKey, value, 30*time.Minute).Err(); err != nil {
 		return fmt.Errorf("%s: redis set: %w", op, err)
+	}
+
+	if err := r.rdb.Del(ctx, countKey).Err(); err != nil {
+		return fmt.Errorf("%s: redis del: %w", op, err)
 	}
 
 	return nil
 }
 
-func (r *RDB) GetTasks(ctx context.Context, key string) (string, error) {
+func (r *RDB) GetTasks(ctx context.Context, key *[]byte, sfKey string) (string, error) {
 	const op = "rdb.GetTasks"
 
-	tasks, err := r.rdb.Get(ctx, key).Result()
+	val, err, _ := r.sfGroup.Do(sfKey, func() (any, error) {
+		BuildKey("tasks:cache:", key)
+		cacheKey := unsafe.String(unsafe.SliceData(*key), len(*key))
+
+		tasks, err := r.rdb.Get(ctx, cacheKey).Result()
+		if err != nil {
+			return "", fmt.Errorf("%s: redis get: %w", op, err)
+		}
+
+		return tasks, nil
+	})
+
 	if err != nil {
-		return "", fmt.Errorf("%s: redis get: %w", op, err)
+		return "", fmt.Errorf("%s: get tasks: %w", op, err)
 	}
 
-	return tasks, nil
+	return val.(string), nil
+}
+
+func (r *RDB) DelTask(ctx context.Context, bufCache, bufCount *[]byte) error {
+	const op = "rdb.DelTask"
+
+	BuildKey("tasks:count:", bufCount)
+	countKey := unsafe.String(unsafe.SliceData(*bufCount), len(*bufCount))
+
+	BuildKey("tasks:cache:", bufCache)
+	cacheKey := unsafe.String(unsafe.SliceData(*bufCache), len(*bufCache))
+
+	pipe := r.rdb.Pipeline()
+	pipe.Del(ctx, cacheKey)
+	pipe.Del(ctx, countKey)
+	if _, err := pipe.Exec(ctx); err != nil {
+		return fmt.Errorf("%s: redis pipeline: %w", op, err)
+	}
+
+	return nil
+}
+
+// BuildKey add prefix to key.
+func BuildKey(prefix string, key *[]byte) {
+	origLen := len(*key)
+	prefLen := len(prefix)
+	totalLen := prefLen + origLen
+
+	*key = (*key)[:totalLen]
+
+	copy((*key)[prefLen:], (*key)[:origLen])
+	copy((*key)[:prefLen], prefix)
 }
