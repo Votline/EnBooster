b9e0d5265c3fa5fb7411a7c9e6d6cf2887ca3da0
Votline <quogg2@gmail.com>
2026-07-03 19:54:38 +0300

    feat&fix: help message and reset states
    - Add const 'helpMsg' for admin commands
    - Use flag 'setToNone' for reset state after operation

--- gateway/internal/router/admin.go
+++ gateway/internal/router/admin.go
@@ -5,11 +5,23 @@ import (
 	"fmt"
 
 	sm "enbstr/internal/statemanager"
+	"enbstr/internal/ui"
 
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 	tele "gopkg.in/telebot.v3"
 )
+
+const helpMsg = `
+* task_add - add task. Format json:
+> [{"task":" <full task message> ","level":"<english level>","answer": "<answer(s)>" }]
+* task_del - delete task. Format message:
+> <level> <position>
+* word_add - add word. Format json:
+> [{"word":"<word>","explain":"<explain>","level":"<english level>","first_letter":"<first letter>"}]
+* word_del - delete word. Format message:
+> <word> <serial number>
+`
 
 func (srv *Server) handleAdmin(c tele.Context) error {
 	const op = "router.admin"
@@ -21,7 +33,11 @@ func (srv *Server) handleAdmin(c tele.Context) error {
 
 	var err error
 	var state int8
+	setToNone := false
 	switch c.Message().Text {
+	case "Help":
+		helpMenu := ui.ReplyMenu([]string{"task_add", "task_del", "word_add", "word_del"})
+		err = c.Send(helpMsg, helpMenu)
 	case "task_add":
 		err = srv.sm.SetState(c.Sender().ID, sm.StateTaskAdding)
 	case "task_del":
@@ -43,11 +59,13 @@ func (srv *Server) handleAdmin(c tele.Context) error {
 				return fmt.Errorf("%s: add task: %w", op, err)
 			}
 			err = c.Send(fmt.Sprintf("Inserted tasks: %d", inserted))
+			setToNone = true
 		case sm.StateTaskDeleting:
 			if err = srv.lrnsrv.DelTask(c.Message().Text, reqTrace); err != nil {
 				return fmt.Errorf("%s: delete task: %w", op, err)
 			}
 			err = c.Send("Task deleted")
+			setToNone = true
 		case sm.StateWordAdding:
 			var inserted int32
 			inserted, err = srv.lrnsrv.NewWords(c.Message().Text, reqTrace)
@@ -55,13 +73,21 @@ func (srv *Server) handleAdmin(c tele.Context) error {
 				return fmt.Errorf("%s: add word: %w", op, err)
 			}
 			err = c.Send(fmt.Sprintf("Inserted words: %d", inserted))
+			setToNone = true
 		case sm.StateWordDeleting:
 			if err = srv.lrnsrv.DelWord(c.Message().Text, reqTrace); err != nil {
 				return fmt.Errorf("%s: delete word: %w", op, err)
 			}
 			err = c.Send("Word deleted")
+			setToNone = true
 		default:
 			err = c.Send("Unknown command")
+			setToNone = true
+		}
+	}
+	if setToNone {
+		if err := srv.sm.SetState(c.Message().Sender.ID, sm.StateNone); err != nil {
+			return fmt.Errorf("%s: set state: %w", op, err)
 		}
 	}
 	if err != nil {
