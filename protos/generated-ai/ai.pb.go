2565986363ee648d2551343b874ec5acb11db23c
Votline <quogg2@gmail.com>
2026-07-23 08:33:24 +0300

    fix(ai.proto): use int64 uuid instead of string

--- protos/generated-ai/ai.pb.go
+++ protos/generated-ai/ai.pb.go
@@ -135,7 +135,7 @@ func (x *GenerateTextRes) GetText() string {
 
 type GenerateAudioReq struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
-	Uuid          string                 `protobuf:"bytes,1,opt,name=uuid,proto3" json:"uuid,omitempty"`
+	Uuid          int64                  `protobuf:"varint,1,opt,name=uuid,proto3" json:"uuid,omitempty"`
 	Text          string                 `protobuf:"bytes,2,opt,name=text,proto3" json:"text,omitempty"`
 	RequestTrace  string                 `protobuf:"bytes,3,opt,name=request_trace,json=requestTrace,proto3" json:"request_trace,omitempty"`
 	unknownFields protoimpl.UnknownFields
@@ -172,11 +172,11 @@ func (*GenerateAudioReq) Descriptor() ([]byte, []int) {
 	return file_ai_proto_rawDescGZIP(), []int{2}
 }
 
-func (x *GenerateAudioReq) GetUuid() string {
+func (x *GenerateAudioReq) GetUuid() int64 {
 	if x != nil {
 		return x.Uuid
 	}
-	return ""
+	return 0
 }
 
 func (x *GenerateAudioReq) GetText() string {
@@ -239,7 +239,7 @@ func (x *GenerateAudioRes) GetAudioData() []byte {
 
 type RecognizeAudioReq struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
-	Uuid          string                 `protobuf:"bytes,1,opt,name=uuid,proto3" json:"uuid,omitempty"`
+	Uuid          int64                  `protobuf:"varint,1,opt,name=uuid,proto3" json:"uuid,omitempty"`
 	AudioData     []byte                 `protobuf:"bytes,2,opt,name=audio_data,json=audioData,proto3" json:"audio_data,omitempty"`
 	RequestTrace  string                 `protobuf:"bytes,3,opt,name=request_trace,json=requestTrace,proto3" json:"request_trace,omitempty"`
 	unknownFields protoimpl.UnknownFields
@@ -276,11 +276,11 @@ func (*RecognizeAudioReq) Descriptor() ([]byte, []int) {
 	return file_ai_proto_rawDescGZIP(), []int{4}
 }
 
-func (x *RecognizeAudioReq) GetUuid() string {
+func (x *RecognizeAudioReq) GetUuid() int64 {
 	if x != nil {
 		return x.Uuid
 	}
-	return ""
+	return 0
 }
 
 func (x *RecognizeAudioReq) GetAudioData() []byte {
@@ -442,14 +442,14 @@ const file_ai_proto_rawDesc = "" +
 	"\x0fGenerateTextRes\x12\x12\n" +
 	"\x04text\x18\x01 \x01(\tR\x04text\"_\n" +
 	"\x10GenerateAudioReq\x12\x12\n" +
-	"\x04uuid\x18\x01 \x01(\tR\x04uuid\x12\x12\n" +
+	"\x04uuid\x18\x01 \x01(\x03R\x04uuid\x12\x12\n" +
 	"\x04text\x18\x02 \x01(\tR\x04text\x12#\n" +
 	"\rrequest_trace\x18\x03 \x01(\tR\frequestTrace\"1\n" +
 	"\x10GenerateAudioRes\x12\x1d\n" +
 	"\n" +
 	"audio_data\x18\x01 \x01(\fR\taudioData\"k\n" +
 	"\x11RecognizeAudioReq\x12\x12\n" +
-	"\x04uuid\x18\x01 \x01(\tR\x04uuid\x12\x1d\n" +
+	"\x04uuid\x18\x01 \x01(\x03R\x04uuid\x12\x1d\n" +
 	"\n" +
 	"audio_data\x18\x02 \x01(\fR\taudioData\x12#\n" +
 	"\rrequest_trace\x18\x03 \x01(\tR\frequestTrace\"'\n" +
